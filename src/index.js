11b308e486d730732faaff5676bac958f7401b9a
Linda Vong <lwvong@cs.washington.edu>
2020-08-18 15:15:54 -0700

    implement for loops for grid instead of hardcoding

--- src/index.js
+++ src/index.js
@@ -20,24 +20,23 @@ class Board extends React.Component {
     );
   }
 
+  // need to create children before assigning to parent
+  createGrid = () => {
+    let grid = [];
+    for (let i = 0; i < 3; i++) {
+      let row = [];
+      for (let j = 0; j < 3; j++) {
+        row.push(this.renderSquare(i * 3 + j));
+      }
+      grid.push(<div className="board-row">{ row }</div>);
+    }
+    return grid;
+  }
+
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {this.createGrid()}
       </div>
     );
   }
