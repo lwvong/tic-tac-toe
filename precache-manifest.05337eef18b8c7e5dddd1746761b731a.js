cf47ee22809af8b92a98fb9dbcfa2ed12a8df494
Linda Vong <lwvong@cs.washington.edu>
2020-08-18 16:20:00 -0700

    Updates

---
+++ precache-manifest.05337eef18b8c7e5dddd1746761b731a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f02fbb6b94f25099eb24075257980bf",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "3df68c0c2d683f511fc8",
+    "url": "/tic-tac-toe/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "a2436778992cd372315b",
+    "url": "/tic-tac-toe/static/js/2.b9926c31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.b9926c31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3df68c0c2d683f511fc8",
+    "url": "/tic-tac-toe/static/js/main.351e1574.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
